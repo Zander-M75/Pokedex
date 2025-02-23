5c52a8bb3306c246d15be1248653cba7cc8c7805
Zander <zandermarenberg@gmail.com>
2025-02-23 00:42:01 -0500

    saving commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,7 @@ import '../styles/main.scss';
 
 export default function Home() {
     return (
-        <div className="home-container">
-            <div className="home-container__inner">
-                <div className="home-container__inner-content"><h1>switching to sass, F U</h1></div>
-            </div>
+        <div className="red-rectangle">
         </div>
     )
 }
