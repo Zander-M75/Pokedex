53e1e0c3263721ae0c3f5e765669454f4d049b09
Zander <zandermarenberg@gmail.com>
2025-02-24 13:57:04 -0500

    filter menu layout

---
+++ src/components/common/FilterMenu.jsx
@@ -0,0 +1,23 @@
+import './FilterMenu.scss';
+export default function FilterMenu() {
+    return (
+        <div className="filter-menu">
+            <h2 className="filter-menu__title">Filter Menu</h2>
+            <div className="filter-menu__content">
+                <h3 className="filter-menu__subtitle">Filter by</h3>
+                <div className="filter-menu__category">
+                    <h4 className="filter-menu__category-title">Category</h4>
+                    <ul className="filter-menu__list">
+                        <li className="filter-menu__item">All</li>
+                        <li className="filter-menu__item">Category 1</li>
+                        <li className="filter-menu__item">Category 2</li>
+                        <li className="filter-menu__item">Category 3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    
+    )
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import '../styles/main.scss';
-
+import FilterMenu from '../components/common/FilterMenu';
 export default function Home() {
     return (
-        <div className="red-rectangle">
+        <div>
+            <FilterMenu />
         </div>
     )
 }
