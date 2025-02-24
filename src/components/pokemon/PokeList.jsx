d49c9ef82ecf08a0e6994b54d9aabce9ac454f52
Zander <zandermarenberg@gmail.com>
2025-02-24 18:51:45 -0500

    pokelist gameboy style

--- src/components/pokemon/PokeList.jsx
+++ src/components/pokemon/PokeList.jsx
@@ -3,9 +3,12 @@ import '../../styles/components/pokemon/PokeList.css';
 export default function PokeList() {
     return (
         <div className="poke-list">
+            <div className="poke-list__header">
+                <h2>PokeList</h2>
+            </div>
             <div className="poke-list__screen">
                 {/* Pokemon content will go here later */}
-                <h2>PokeList</h2>
+                
             </div>
         </div>
     )
